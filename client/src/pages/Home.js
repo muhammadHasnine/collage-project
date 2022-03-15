82799e7ef739ba6583d154abf5fe5e3ad6bc7d31
muhammadHasnine <mdhasninesheak@gmail.com>
2022-03-15 09:54:39 +0600

    tech hover and student detail

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,18 +8,30 @@ const Home = () => {
         <StudentCorner img={"images/student-corner.png"} pagelink={"/stregis"}/>
         <div className="right_menu w-[250px]">
             <StudentCorner className='menu_bar' img={"https://mechomotive.com/wp-content/uploads/2021/07/Modren-Tech.jpg"} pagelink={"/"}/>
-            <div className="drop_down hidden list-none cursor-pointer">
+            <div className="drop_down hidden list-none cursor-pointer bg-gradient-to-t from-[#ffffff] to-[#ffc6c6] mb-[30px]">
                 <Link to={`/studetail/${"Computer"}`}>
-                    <li>Computer</li>
+                    <li className='p-[10px] hover:bg-gradient-to-r from-[#daffda] to-[white] hover:text-[#333] hover:duration-1000 font-semibold text-[white]'>Computer</li>
                 </Link>
                 <Link to={`/studetail/${"Civil"}`}>
-                <li>Civil</li>
+                <li className='p-[10px] hover:bg-gradient-to-r from-[#daffda] to-[white] hover:text-[#333] font-semibold text-[white] hover:duration-1000'>Civil</li>
                 </Link>
                 <Link to={`/studetail/${"Electrical"}`}>
-                    <li>Electrical</li>
+                    <li className='p-[10px] hover:bg-gradient-to-r from-[#daffda] to-[white] hover:text-[#333] font-semibold text-[white] hover:duration-1000'>Electrical</li>
                 </Link>
                 <Link to={`/studetail/${"Electronics"}`}>
-                    <li>Electronics</li>
+                    <li className='p-[10px] hover:bg-gradient-to-r from-[#daffda] to-[white] hover:text-[#333] font-semibold text-[white] hover:duration-1000'>Electronics</li>
+                </Link>
+                <Link to={`/studetail/${"Power"}`}>
+                    <li className='p-[10px] hover:bg-gradient-to-r from-[#daffda] to-[white] hover:text-[#333] font-semibold text-[#686565] hover:duration-1000'>Power</li>
+                </Link>
+                <Link to={`/studetail/${"ENVT"}`}>
+                    <li className='p-[10px] hover:bg-gradient-to-r from-[#daffda] to-[white] hover:text-[#333] font-semibold text-[#686565] hover:duration-1000'>ENVT</li>
+                </Link>
+                <Link to={`/studetail/${"IPCT"}`}>
+                    <li className='p-[10px] hover:bg-gradient-to-r from-[#daffda] to-[white] hover:text-[#333] font-semibold text-[#686565] hover:duration-1000'>IPCT</li>
+                </Link>
+                <Link to={`/studetail/${"Automobile"}`}>
+                    <li className='p-[10px] hover:bg-gradient-to-r from-[#daffda] to-[white] hover:text-[#333] font-semibold text-[#686565] hover:duration-1000'>Automobile</li>
                 </Link>
                 
             </div>
--- client/src/pages/StudentDetail.js
+++ client/src/pages/StudentDetail.js
@@ -11,11 +11,34 @@ const StudentDetail = () => {
     },[])
     const filterstudata = studata.filter((c)=>c.technology===tech)
   return (
-    <div>{tech} Technology Students data
-    {filterstudata.map((s)=>{
-        return <p>{s.studentName}</p>
-    })}
+    <>
+    <div className="container border-[1px] border-[#6db700] bg-gradient-to-t from-[#ededed] to-[#fefefe] rounded-[5px] max-w-[90%] md:max-w-[80%] m-[30px_auto] p-[15px]">
+    <div className="title bg-[#6db700] p-[10px] rounded-[5px_5px_0px_0px] text-center text-[white]">
+        <p>{tech} বিভাগের ছাত্র-ছাত্রী</p>
+      </div>
+        <div className="hdtitle mt-[10px] bg-gradient-to-t from-[#c2f0ea] to-[#fefefe]">
+          <ul className='flex justify-around'>
+            <li className='p-[10px] font-semibold '>Student Name</li>
+            <li className='p-[10px] font-semibold'>Semester</li>
+            <li className='p-[10px] font-semibold'>Shift</li>
+            <li className='p-[10px] font-semibold'>Session</li>
+            <li className='p-[10px] font-semibold'>Gender</li>
+          </ul>
+        </div>
+        <div className="stu_data">
+          {filterstudata.map((s)=>{
+              return <ul className='flex justify-around bg-[#ffffff] m-[5px_0px]'>
+                        <li className='p-[10px]'>{s.studentName}</li>
+                        <li className='p-[10px]'>{s.semester}</li>
+                        <li className='p-[10px]'>{s.shift}</li>
+                        <li className='p-[10px]'>{s.session}</li>
+                        <li className='p-[10px]'>{s.gender}</li>
+                    </ul>
+          })}
+          
+        </div>
     </div>
+    </>
   )
 }
 
